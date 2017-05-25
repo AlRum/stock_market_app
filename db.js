023fcd93f096757445ea5a30488b017e9974e43b
AlRum <shiroc5@gmail.com>
2017-05-25 22:10:10 +0000

    working_version

--- db.js
+++ db.js
@@ -1,4 +1,5 @@
 module.exports = {
 	//'url' : 'mongodb://<dbuser>:<dbpassword>@novus.modulusmongo.net:27017/<dbName>'
-	'url' : 'mongodb://localhost/test'
+	//'url' : 'mongodb://localhost/test'
+	'url' : 'mongodb://shiroc:123@ds153501.mlab.com:53501/voting_app'
 }
