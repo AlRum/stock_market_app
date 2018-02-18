f8f7568a33bb4df27706719116a8f8e49fe0b73a
AlRum <shiroc5@gmail.com>
2018-02-18 08:24:22 +0000

    working version

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var app = express();
 
 app.use(cookieSession({
   name: 'session',
-  keys: [/* secret keys */],
+  keys: ['asdsad', 'asdsad12312'],
 
   // Cookie Options
   maxAge: 24 * 60 * 60 * 1000 // 24 hours
